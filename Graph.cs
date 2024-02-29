682457b37c314aef8a03f738ef38d0269a92e6c6
Thien Nguyen <thiennguyen03001@gmail.com>
2024-02-29 16:13:31 +0700

    Add project files.

---
+++ Graph.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Graph
+{
+    public class Graph<T> // dùng đồ thị có hướng
+    {
+        private Dictionary<T, List<T>> edgesList;
+        public Graph()
+        {
+            edgesList = new Dictionary<T, List<T>>();
+        }
+        public void AddEdge(T from, T to)
+        {
+            if (!edgesList.ContainsKey(from))
+                edgesList[from] = new List<T>();
+            edgesList[from].Add(to);
+        }
+        public void PrintGraph()
+        {
+            foreach (var vertex in edgesList)
+            {
+                Console.Write(vertex.Key + " -> ");
+                foreach (var neighbor in vertex.Value)
+                {
+                    Console.Write(neighbor + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        private List<T> BFS(T from)
+        {
+            Dictionary<T, bool> visited = new Dictionary<T, bool>();
+            List<T> result = new List<T>();
+            Queue<T> queue = new Queue<T>();
+
+            visited[from] = true;
+            queue.Enqueue(from);
+
+            while (queue.Count > 0)
+            {
+                T current = queue.Dequeue();
+                result.Add(current);
+
+                if (!edgesList.ContainsKey(current)) // nếu ko có tổ tiên, bỏ qua
+                    continue;
+
+                foreach (T child in edgesList[current])
+                {
+                    if (!visited.ContainsKey(child) || !visited[child])
+                    {
+                        visited[child] = true;
+                        queue.Enqueue(child);
+                    }
+                }
+            }
+
+            return result;
+        }
+        public void PrintBFS(T from)
+        {
+            List<T> bfs = new List<T>();
+            bfs = BFS(from);
+            foreach(var i in bfs)
+            {
+                Console.Write(i+" ");
+            }
+        }
+        private List<T> DFS(T from)
+        {
+            Stack<T> dfs = new Stack<T>();
+            Dictionary<T, bool> visited = new Dictionary<T, bool>();
+            List<T> result = new List<T>();
+
+            dfs.Push(from);
+            visited[from] = true; // Đánh dấu nút bắt đầu đã được thăm 
+
+            while (dfs.Count > 0)
+            {
+                T current = dfs.Pop(); // Rút đỉnh khỏi ngăn xếp
+
+                result.Add(current); 
+
+                if (!edgesList.ContainsKey(current)) // nếu ko có tổ tiên, bỏ qua
+                    continue;
+
+                foreach (T child in edgesList[current]) // Duyệt qua tất cả các con của nút hiện tại
+                {
+                    if (!visited.ContainsKey(child) || !visited[child]) // Nếu một nút con chưa được thăm
+                    {
+                        dfs.Push(child); 
+                        visited[child] = true; 
+                    }
+                }
+            }
+            return result; 
+        }
+        public void PrintDFS(T from)
+        {
+            List<T> res = new List<T>();
+            res = DFS(from);
+            foreach (var edge in res)
+            {
+                Console.Write(edge + " ");
+            }
+        }
+        public void TopoSort()
+        {
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Graph
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph<int> g = new Graph<int>();
+
+            g.AddEdge(0, 1);
+            g.AddEdge(1, 2);
+            g.AddEdge(2, 3);
+            g.AddEdge(0, 4);
+            g.AddEdge(4, 5);
+            g.AddEdge(5, 6);
+            g.AddEdge(0, 7);
+            g.AddEdge(7, 8);
+            g.AddEdge(8, 9);
+            g.AddEdge(0, 10);
+            g.AddEdge(10, 11);
+            g.AddEdge(11, 12);
+            //g.PrintGraph();
+            g.PrintDFS(0);
+        }
+    }
+}
